e48363b31c4ead297fad78f0cd1968967c717512
Nitesh511 <69584724+Nitesh511@users.noreply.github.com>
2023-05-14 17:26:18 +0545

    commit for calculator

--- calculator.py
+++ calculator.py
@@ -1,99 +1,80 @@
 from tkinter import *
 from tkinter import messagebox
 
-class my_calculator:
-    def __init__(self,root):
-        self.root=root
-        self.root.title("Calculator")
-        self.root.geometry('350x420')
+cal = Tk()                      
+cal.geometry("312x394")         
+cal.resizable(0, 0)             
+cal.title("Calculator")
 
-        self.expression=''
-        self.txtinput=StringVar()
+def click_button(item):                                         
+    global expression
+    inputText.set(inputText.get()+(str(item)))
 
-        ent_display=Entry(self.root,bd=7,bg='light blue',textvariable=self.txtinput,relief=SUNKEN,justify=RIGHT,font=('times new roman',25,'bold'))
-        ent_display.grid(row=0,columnspan=4)
+def clear_button():                                              
+    global expression
+    expression = ""
+    inputText.set(inputText.get()[0:-1])
 
-        btn9=Button(self.root,text='9',bd=15,font=('times new roman',15,'bold'),bg='orange',\
-                    command=lambda :self.btn_click(9))
-        btn9.grid(row=1,padx=10,pady=10)
+def clear_all():                                                  
+    inputText.set("")
 
-        btn8 = Button(self.root, text='8', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click(8))
-        btn8.grid(row=1, column=1, padx=10, pady=10)
+def equal_button():
+    result = ""
+    try:
+        result = eval(inputText.get())                             
+        inputText.set(result)
+    except:
+        result = "_ERROR_"
+        messagebox.showerror("Invalid Input", "Please enter a valid arithmetic expression.")
 
-        btn7= Button(self.root, text='7', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click(7))
-        btn7.grid(row=1,column=2, padx=10, pady=10)
+expression = ""
+inputText = StringVar()                   
 
-        btn6 = Button(self.root, text='6', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click(6))
-        btn6.grid(row=2,column=0, padx=10, pady=10)
+#Input Frame
+inputFrame = Frame(cal, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="crimson", highlightthickness=2)
+inputFrame.pack(side=TOP)
+inputField = Entry(inputFrame, font=('arial', 19 ), textvariable=inputText, width=50,fg="black", bg="white", bd=0, justify=RIGHT)
+inputField.grid(row=0, column=0)
+inputField.pack(ipady=19)
 
-        btn5 = Button(self.root, text='5', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click(5))
-        btn5.grid(row=2,column=1, padx=10, pady=10)
+#Button Frame
+button_frame = Frame(cal, width=312, height=272.5, bg="grey")
+button_frame.pack()
 
-        btn4 = Button(self.root, text='4', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click(4))
-        btn4.grid(row=2, column=2, padx=10, pady=10)
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Buttons>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+#First_row
+clearall = Button(button_frame, text = "C", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: clear_all()).grid(row = 1, column = 0, padx = 1, pady = 1)
+l_bracket= Button(button_frame, text = "(", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button("(")).grid(row = 1, column = 1, padx = 1, pady = 1)
+r_bracket = Button(button_frame, text = ")", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button(")")).grid(row = 1, column = 2, padx = 1, pady = 1)
+clear = Button(button_frame, text = "<", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: clear_button()).grid(row = 1, column = 3, padx = 1, pady = 1)
 
-        btn3 = Button(self.root, text='3', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click(3))
-        btn3.grid(row=3, column=0, padx=10, pady=10)
+#Second_row
+power = Button(button_frame, text = "^", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button("**")).grid(row = 2, column = 0, padx = 1, pady = 1)
+pie = Button(button_frame, text = "π", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button(3.1415)).grid(row = 2, column = 1, padx = 1, pady = 1)
+exp  = Button(button_frame, text = "e", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button(2.7182)).grid(row = 2, column = 2, padx = 1, pady = 1)
+divide_= Button(button_frame, text = "/", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button("/")).grid(row = 2, column = 3, padx = 1, pady = 1)
 
-        btn2 = Button(self.root, text='2', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click(2))
-        btn2.grid(row=3, column=1, padx=10, pady=10)
+#Third_row
+seven = Button(button_frame, text = "7", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(7)).grid(row = 3, column = 0, padx = 1, pady = 1)
+eight = Button(button_frame, text = "8", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(8)).grid(row = 3, column = 1, padx = 1, pady = 1)
+nine = Button(button_frame, text = "9", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(9)).grid(row = 3, column = 2, padx = 1, pady = 1)
+multiply = Button(button_frame, text = "*", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button("*")).grid(row = 3, column = 3, padx = 1, pady = 1)
 
-        btn1 = Button(self.root, text='1', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click(1))
-        btn1.grid(row=3, column=2, padx=10, pady=10)
+#Fourth_row
+four = Button(button_frame, text = "4", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(4)).grid(row = 4, column = 0, padx = 1, pady = 1)
+five = Button(button_frame, text = "5", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(5)).grid(row = 4, column = 1, padx = 1, pady = 1)
+six = Button(button_frame, text = "6", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(6)).grid(row = 4, column = 2, padx = 1, pady = 1)
+minus = Button(button_frame, text = "-", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button("-")).grid(row = 4, column = 3, padx = 1, pady = 1)
 
-        btn0= Button(self.root, text='0', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                     command=lambda :self.btn_click(0))
-        btn0.grid(row=4, column=0, padx=10, pady=10)
+#Fifth_row
+one = Button(button_frame, text = "1", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(1)).grid(row = 5, column = 0, padx = 1, pady = 1)
+two = Button(button_frame, text = "2", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(2)).grid(row = 5, column = 1, padx = 1, pady = 1)
+three = Button(button_frame, text = "3", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(3)).grid(row = 5, column = 2, padx = 1, pady = 1)
+plus = Button(button_frame, text = "+", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button("+")).grid(row = 5, column = 3, padx = 1, pady = 1)
 
-        btndive= Button(self.root, text='/', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click('/'))
-        btndive.grid(row=4, column=1, padx=10, pady=10)
-
-        btnequal = Button(self.root, text='=', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_equal())
-        btnequal.grid(row=4, column=2, padx=10, pady=10)
-
-        btnAC= Button(self.root, text='Clear', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_clr())
-        btnAC.grid(row=1,column=3, padx=10, pady=10)
-
-        btnsub = Button(self.root, text='-', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click('-'))
-        btnsub.grid(row=2,column=3, padx=10, pady=10)
-
-        btnmulti = Button(self.root, text='*', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click('*'))
-        btnmulti.grid(row=3,column=3, padx=10, pady=10)
-
-        btnADD = Button(self.root, text='+', bd=15, font=('times new roman', 15, 'bold'), bg='orange',\
-                    command=lambda :self.btn_click('+'))
-        btnADD.grid(row=4,column=3, padx=10, pady=10)
-
-    def btn_click(self,number):
-        self.expression=self.expression+str(number)
-        self.txtinput.set(self.expression)
-
-    def btn_equal(self):
-        try:
-            cal=str(eval(self.expression))
-            self.txtinput.set(cal)
-            self.expression=''
-        except:
-            messagebox.showerror("Invalid Input", "Please enter a valid arithmetic expression.")
-
-    def btn_clr(self):
-        self.txtinput.set('')
-        self.expression=''
-
-
-tk=Tk()
-my_calculator(tk)
-mainloop()
+#Sixth_row
+point = Button(button_frame, text = ".", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button(".")).grid(row = 6, column = 0, padx = 1, pady = 1)
+zero = Button(button_frame, text = "0", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(0)).grid(row = 6, column = 1, padx = 1, pady = 1)
+equals = Button(button_frame, text = "=", fg = "crimson", width = 21, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: equal_button()).grid(row = 6, column = 2, columnspan = 2, padx = 1, pady = 1)
+ 
+cal.mainloop()                    
