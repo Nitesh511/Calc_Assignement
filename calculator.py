e23d8408ad21da7c3f9141ae8e63ab809f9c8e4c
Nitesh511 <69584724+Nitesh511@users.noreply.github.com>
2023-05-15 11:50:10 +0545

    commit for assignement

--- calculator.py
+++ calculator.py
@@ -1,53 +1,52 @@
-from tkinter import *
-from tkinter import messagebox
+from tkinter import * #python version 3.11.1
+from tkinter import messagebox #importing message box to show error message
 
-cal = Tk()                      
-cal.geometry("312x394")         
-cal.resizable(0, 0)             
-cal.title("Calculator")
+cal = Tk()
+cal.geometry("312x394") #size of the calculator 
+cal.resizable(0, 0)
+cal.title("Calculator") #title of the application 
 
-def click_button(item):                                         
+def click_button(item):
     global expression
-    inputText.set(inputText.get()+(str(item)))
+    inputText.set(inputText.get() + (str(item)))  # Concatenates the clicked button's value to the expression
 
-def clear_button():                                              
+def clear_button():
     global expression
     expression = ""
-    inputText.set(inputText.get()[0:-1])
+    inputText.set(inputText.get()[0:-1])  # Removes the last character from the expression
 
-def clear_all():                                                  
-    inputText.set("")
+def clear_all():
+    inputText.set("")  # Clears the entire expression
 
 def equal_button():
     result = ""
     try:
-        result = eval(inputText.get())                             
-        inputText.set(result)
+        result = eval(inputText.get())  # Evaluates the expression and stores the result
+        inputText.set(result)  # Displays the result in the input field
     except:
         result = "_ERROR_"
         messagebox.showerror("Invalid Input", "Please enter a valid arithmetic expression.")
 
-expression = ""
-inputText = StringVar()                   
+expression = ""  # Stores the arithmetic expression
+inputText = StringVar()  # Variable to store the text displayed in the input field
 
-#Input Frame
-inputFrame = Frame(cal, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="crimson", highlightthickness=2)
+# Create a frame for the input field
+inputFrame = Frame(cal, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="blue", highlightthickness=2)
 inputFrame.pack(side=TOP)
-inputField = Entry(inputFrame, font=('arial', 19 ), textvariable=inputText, width=50,fg="black", bg="white", bd=0, justify=RIGHT)
+inputField = Entry(inputFrame, font=('arial', 19), textvariable=inputText, width=50, fg="black", bg="white", bd=0, justify=RIGHT) # Create an entry field for the input
 inputField.grid(row=0, column=0)
 inputField.pack(ipady=19)
 
-#Button Frame
+# Button Frame
 button_frame = Frame(cal, width=312, height=272.5, bg="grey")
 button_frame.pack()
 
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Buttons>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
-#First_row
-clearall = Button(button_frame, text = "C", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: clear_all()).grid(row = 1, column = 0, padx = 1, pady = 1)
-l_bracket= Button(button_frame, text = "(", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button("(")).grid(row = 1, column = 1, padx = 1, pady = 1)
-r_bracket = Button(button_frame, text = ")", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button(")")).grid(row = 1, column = 2, padx = 1, pady = 1)
-clear = Button(button_frame, text = "<", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: clear_button()).grid(row = 1, column = 3, padx = 1, pady = 1)
-
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Buttons>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# First row
+clearall = Button(button_frame, text="C", fg="white", width=10, height=3, bd=0, bg="black", cursor="hand2", command=lambda: clear_all()).grid(row=1, column=0, padx=1, pady=1)
+l_bracket = Button(button_frame, text="(", fg="white", width=10, height=3, bd=0, bg="black", cursor="hand2", command=lambda: click_button("(")).grid(row=1, column=1, padx=1, pady=1)
+r_bracket = Button(button_frame, text=")", fg="white", width=10, height=3, bd=0, bg="black", cursor="hand2", command=lambda: click_button(")")).grid(row=1, column=2, padx=1, pady=1)
+clear = Button(button_frame, text="<", fg="white", width=10, height=3, bd=0, bg="black", cursor="hand2", command=lambda: clear_button()).grid(row=1, column=3, padx=1, pady=1)
 #Second_row
 power = Button(button_frame, text = "^", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button("**")).grid(row = 2, column = 0, padx = 1, pady = 1)
 pie = Button(button_frame, text = "π", fg = "white", width = 10, height = 3, bd = 0, bg = "black", cursor = "hand2", command = lambda: click_button(3.1415)).grid(row = 2, column = 1, padx = 1, pady = 1)
